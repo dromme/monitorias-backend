3424261edc9045ef88ddc5b321f7f6e69073f7eb
Luis <eduardo.leo8a@hotmail.com>
2019-09-16 21:43:47 -0500

    adding get all asesorias by student

--- App/routes/getAsesoria.js
+++ App/routes/getAsesoria.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const Asesoria = require('../utils/searchAsesoria');
+const Subscribe = require('../utils/subscription');
 
 router.get('/byDay',Asesoria.getAsesoriaByDay);
 router.get('/byMonth',Asesoria.getAsesoriaByMonth);
 router.post('/createAsesoria',Asesoria.createAsesoria);
 router.get('/byInstructor',Asesoria.getAsesoriaByInstructor);
 router.get('/getAllAsesorias',Asesoria.getAllAsesorias);
+router.get('/getAsesoriasByStudent',Subscribe.getAsesoriasByStudent);
     
 module.exports = router;
